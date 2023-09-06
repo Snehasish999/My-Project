fbd1d4e267825434e7a8663ddc733b7528967555
Sanji101 <snehasishdas@branchkarma.com>
2023-09-07 01:03:07 +0530

    section added hero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,26 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
-import { MenuComponent } from './menu/menu.component';
+
 import { MenuPageComponent } from './menu-page/menu-page.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { ReviewComponent } from './review/review.component';
 import { FooterComponent } from './footer/footer.component';
 import { WhyUsComponent } from './why-us/why-us.component';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    MenuComponent,
+    
     MenuPageComponent,
     AboutComponent,
     ContactComponent,
     ReviewComponent,
     FooterComponent,
-    WhyUsComponent
+    WhyUsComponent,
+    HeroSectionComponent
   ],
   imports: [
     BrowserModule,
